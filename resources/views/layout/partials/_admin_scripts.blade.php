9ed532aaffd2afaafa75fe4fab0953f243868eef
Jordan <moy27pogi@gmail.com>
2018-06-13 14:44:26 +0800

    CRUD in Featured News

--- resources/views/layout/partials/_admin_scripts.blade.php
+++ resources/views/layout/partials/_admin_scripts.blade.php
@@ -2,3 +2,4 @@
 <script src="{{ asset('/js/pages/events.js') }}"></script>
 <script src="{{ asset('/plugins/toast-master/js/jquery.toast.js') }}"></script>
 <script src="{{ asset('/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('/plugins/ckeditor/ckeditor.js') }}"></script>
